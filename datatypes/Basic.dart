7891e10d361b07349cbac8b677aec504e69fb707
sunyeon-Jeong <sunyeon4012@gmail.com>
2025-05-01 16:14:29 +0900

    Feat : datatypes_basic

---
+++ datatypes/Basic.dart
@@ -0,0 +1,13 @@
+// 타입들 대부분이 object, function도 object
+
+void main() {
+  String name = "mallang";
+  bool alive = true;
+  int age = 24;
+  double money = 11.11;
+
+  // num : int, double 자료형 둘 다 올 수 있을 때
+  // num : integer와 double의 부모 class
+  num x = 22;
+  x = 24;
+}
