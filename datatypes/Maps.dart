e4bd9065961345d96e416d111f4fcc61821ee069
sunyeon-Jeong <sunyeon4012@gmail.com>
2025-05-01 18:06:27 +0900

    Feat : functions_DefiningFunction

---
+++ datatypes/Maps.dart
@@ -0,0 +1,13 @@
+void main() {
+  // Map<String, Object>
+  var character = {
+    'name' : 'mallang',
+    'xp' : 19.99,
+    'superpower' : false,
+  };
+
+  Map<int, bool> boolean = {
+    1 : true,
+    0 : false
+  };
+}
